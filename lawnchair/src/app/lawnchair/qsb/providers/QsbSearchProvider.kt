73fca1a33704dd8e74f290f62653a953cf1c9188
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2022-10-17 14:16:19 +0800

    Create Brave QSB Provider (closes #2706)

--- lawnchair/src/app/lawnchair/qsb/providers/QsbSearchProvider.kt
+++ lawnchair/src/app/lawnchair/qsb/providers/QsbSearchProvider.kt
@@ -51,7 +51,8 @@ open class QsbSearchProvider(
             DuckDuckGo,
             Presearch,
             Wikipedia,
-            Bing
+            Bing,
+            Brave
         )
 
         /**
