264ab9d7ad35e467ebc34e16b6dcb9e05cfd0c6f
validcube <pun.butrach@gmail.com>
2023-12-19 20:58:25 +0700

    Refactor Battery provider to use preferenceManager2
    Signed-off-by: validcube <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/smartspace/provider/BatteryStatusProvider.kt
+++ lawnchair/src/app/lawnchair/smartspace/provider/BatteryStatusProvider.kt
@@ -6,6 +6,7 @@ import android.content.IntentFilter
 import android.graphics.drawable.Icon
 import android.os.BatteryManager
 import androidx.core.content.getSystemService
+import app.lawnchair.preferences2.PreferenceManager2
 import app.lawnchair.smartspace.model.SmartspaceAction
 import app.lawnchair.smartspace.model.SmartspaceScores
 import app.lawnchair.smartspace.model.SmartspaceTarget
@@ -13,13 +14,16 @@ import app.lawnchair.util.broadcastReceiverFlow
 import app.lawnchair.util.formatShortElapsedTimeRoundingUpToMinutes
 import com.android.launcher3.R
 import com.android.launcher3.Utilities
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+
 
 class BatteryStatusProvider(context: Context) : SmartspaceDataSource(
     context,
     R.string.smartspace_battery_status,
     { smartspaceBatteryStatus },
 ) {
+    private val prefs2 = PreferenceManager2.getInstance(context)
     private val batteryManager = context.getSystemService<BatteryManager>()
 
     override val internalTargets = broadcastReceiverFlow(context, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
@@ -35,14 +39,25 @@ class BatteryStatusProvider(context: Context) : SmartspaceDataSource(
             listOfNotNull(getSmartspaceTarget(charging, full, level))
         }
 
-    private fun getSmartspaceTarget(charging: Boolean, full: Boolean, level: Int): SmartspaceTarget? {
+    private suspend fun getSmartspaceTarget(charging: Boolean, full: Boolean, level: Int): SmartspaceTarget? {
+        val prioritizeLowBattery = prefs2.smartspacePrioritizeLowBattery.get().map{it}.first()
+        val lowBatteryTrigger = prefs2.smartspaceBatteryLowPriority.get().map{it}.first()
+
         val title = when {
             full || level == 100 -> return null
             charging -> context.getString(R.string.smartspace_battery_charging)
-            level <= 15 -> context.getString(R.string.smartspace_battery_low)
+            level <= lowBatteryTrigger -> context.getString(R.string.smartspace_battery_low)
             else -> return null
         }
-        val score = if (level <= 15) {
+
+        /*
+        TODO(@validcube): The name of the variable doesn't make sense,
+              Consider checking out once the changes is PR-ready.
+                1) prioritizeLowBattery could be isPrioritizeLowBattery
+                2) lowBatteryTrigger could be ... *something*
+              Don't forget to checkout prefs name
+        */
+        val score = if (prioritizeLowBattery || (level <= lowBatteryTrigger)) {
             SmartspaceScores.SCORE_LOW_BATTERY
         } else {
             SmartspaceScores.SCORE_BATTERY
